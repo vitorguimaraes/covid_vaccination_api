ab51d6ffbfeb3bbe34825fe4b264989188677a83
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2022-10-31 20:32:12 -0300

    chore: 🔧 create iex script

---
+++ .iex.exs
@@ -0,0 +1,9 @@
+person = %{
+  name: "John Bovi",
+  birth: ~D[1978-06-25],
+  cpf: "12345678900",
+  mother_name: "Mary",
+  genre: "M",
+  email: "john@mail.com",
+  password: "supersecretpassword"
+}
